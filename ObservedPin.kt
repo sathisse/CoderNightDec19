ed30c1f9cdb50b0ce0dd3897de8b2b8b4999de12
scottt <scottt@centripetalfusion.com>
2019-12-11 20:27:57 -0500

    Remove redundant local version of result list

--- ObservedPin.kt
+++ ObservedPin.kt
@@ -1,26 +1,25 @@
-package com.example.myapplication
+class ObservedPinKt {
+    companion object {
+        private val possibles = arrayOf("08", "124", "2135", "326", "4157",
+                                        "52468", "6359", "748", "80579", "968")
+        private var result: MutableList<String> = mutableListOf()
 
+        fun getPINs(observed: String): List<String> {
+            result = mutableListOf()
+            buildPinStrings("", observed)
+            return result
+        }
 
-object ObservedPin {
-    private val possibles = arrayOf("08", "124", "2135", "326", "4157",
-                                    "52468", "6359", "748", "80579", "968")
-    private val result: MutableList<String> = mutableListOf()
+        private fun buildPinStrings(partial: String, remaining: String) {
+            // If there are no more digits, add the pin string to the result:
+            if (remaining.isEmpty()) {
+                result.add(partial)
 
-    fun getPINs(observed: String): List<String> {
-        val result = mutableListOf<String>()
-        buildPinStrings("", observed)
-        return result
-    }
-
-    private fun buildPinStrings(partial: String, remaining: String) {
-        // If there are no more digits, add the pin string to the result:
-        if (remaining.isEmpty()) {
-            result.add(partial)
-
-        } else {
-            // Loop through the possible digits and recursively build the remainder of the pin string:
-            for (element in possibles[remaining[0] - '0']) {
-                buildPinStrings(partial + element, remaining.substring(1))
+            } else {
+                // Loop through the possible digits and recursively build the remainder of the pin string:
+                for (element in possibles[remaining[0] - '0']) {
+                    buildPinStrings(partial + element, remaining.substring(1))
+                }
             }
         }
     }
